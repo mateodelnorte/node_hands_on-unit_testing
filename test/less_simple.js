16c2afc9b210cf3712e48d11dee77c17217cce00
Matt Walters <mattwalters5@gmail.com>
2012-08-08 10:39:14 -0400

    filling out mockrequire

--- test/less_simple.js
+++ test/less_simple.js
@@ -1,8 +1,9 @@
 require('should');
 
-var somethingInteresting = require('../lib/somethingInteresting');
-
 describe('somethingInteresting', function () {
+
+  var somethingInteresting = require('../lib/somethingInteresting');
+  
   describe('#funkytion', function () {
     it('should return the value from the dependency', function (done) {
       somethingInteresting.funkytion(function (err, result) {
@@ -15,13 +16,16 @@ describe('somethingInteresting', function () {
 
 var mockrequire = require('mockrequire');
 
-mockrequire('../lib/somethingInteresting');
-
 describe('somethingInteresting', function () {
+
+  somethingInteresting = mockrequire('../lib/somethingInteresting', {
+    './dependency': function (callback) { callback(null, 'test'); }
+  });
+
   describe('#funkytion', function () {
     it('should return what we say, and *like* it!', function (done) {
       somethingInteresting.funkytion(function (err, result) {
-        result.should.eql('I really don\'t want to have to call this method. What if it was an external API? KnowwhatI\mtalkinabout?');
+        result.should.eql('test KnowwhatI\mtalkinabout?');
         done();
       });
     });
